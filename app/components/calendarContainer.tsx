9c01b6760eadcc287a8625de3e69dede2ef91abd
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-15 01:31:58 -0400

    fixed

--- app/components/calendarContainer.tsx
+++ app/components/calendarContainer.tsx
@@ -8,23 +8,27 @@ import listPlugin from "@fullcalendar/list";
 
 const CalendarContainer = ( { events }:{ events: CalendarEvent[] } ): React.JSX.Element => {
   const getAspectRatio = () => {
-    if ( window.innerWidth < 640 ) {
-      return 0.5;
-    } else if ( window.innerWidth < 1024 ) {
-      return 1;
-    } else if ( window.innerWidth > 2222 ) {
-      return 2;
+    if ( typeof window !== "undefined" ) {
+      if ( window.innerWidth < 640 ) {
+        return 0.5;
+      } else if ( window.innerWidth < 1024 ) {
+        return 1;
+      } else if ( window.innerWidth > 2222 ) {
+        return 2;
+      }
+      else {
+        return 1.5;
+      }
     }
-    else {
-      return 1.5;
-    }
+    return 1.5;
   };
   const [ aspectRatio, setAspectRatio ] = useState ( getAspectRatio() );
 
-  //change aspect ratio based for large medium and small screens
-  window.addEventListener( "resize", () => {
-    setAspectRatio( getAspectRatio() );
-  } );
+  if ( typeof window !== "undefined" ) {
+    window.addEventListener( "resize", () => {
+      setAspectRatio( getAspectRatio() );
+    } );
+  }
   return (
     <div className="w-full sm:w-7/8 2xl:w-3/4 mx-auto">
       <FullCalendar
