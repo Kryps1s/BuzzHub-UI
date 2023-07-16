297a2bac1ab83b5d90a7ff42bb4c2b09e4d9b8ab
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-16 12:23:13 -0400

    base signup form with lots of room for improvement

---
+++ app/signup/page.tsx
@@ -0,0 +1,15 @@
+import { NextPage, Metadata } from "next";
+import SignUpForm from "../components/signUpForm";
+
+export const metadata: Metadata = {
+  title: "Sign Up To Buzzhub"
+};
+
+const Page: NextPage = () => (
+  <main className="h-full flex flex-column items-center  justify-center">
+    <div className="w-1/2 h-1/2">
+      <SignUpForm />
+    </div>
+  </main>
+);
+export default Page;
