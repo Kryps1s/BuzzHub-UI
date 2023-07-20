5d5e70d763e987141e2f90edcd9d58bd0e5bda89
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-20 15:06:55 -0400

    error handling

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -5,7 +5,19 @@ import { EventType, Event } from "../lib/types";
 import { use } from "react";
 
 const Page: NextPage = () => {
-  const events = use ( getData() ).map( ( event: Event ) => ( {
+  const events = use ( getData() );
+  if( events.error ) {
+    return (
+      <Layout>
+        <div className="w-full sm:w-7/8 2xl:w-3/4 mx-auto">
+          <h1 className="text-2xl text-center">Error</h1>
+          <p className="text-center">{events.error}</p>
+        </div>
+      </Layout>
+    );
+  }
+  events.data = events.data
+  .map( ( event: Event ) => ( {
     title:
       event.name
         ? event.name
@@ -18,7 +30,7 @@ const Page: NextPage = () => {
 
   return (
     <Layout>
-      <CalendarContainer events={events} />
+      <CalendarContainer events={events.data} />
     </Layout>
   );
 };
@@ -56,12 +68,19 @@ const getData = async () => {
       } )
     } );
     const json = await res.json();
-    const results = [];
-    results.push( ...json.data.getEvents );
-    return results;
+    //check for graphQl errors
+    if ( json.errors ) {
+      console.error( json.errors );
+      return {
+        error: json.errors[0].message
+      };
+    }
+    return {data:json.data.getEvents};
   } else {
     console.error( "API_URL and API_KEY not set" );
-    return [];
+    return {
+      error: "API_URL and API_KEY not set"
+    };
   }
 };
 export default Page;
