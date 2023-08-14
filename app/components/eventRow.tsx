81425dae4f2baa983042857cec4f789724a0e790
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-08-14 14:33:44 -0400

    upcoming and past events

--- app/components/eventRow.tsx
+++ app/components/eventRow.tsx
@@ -4,24 +4,28 @@ import { EventCard } from "../lib/types";
 import Link from "next/link";
 
 interface EventRowProps {
-    events: EventCard[];
+    events: EventCard[] | {past: EventCard[], upcoming: EventCard[]};
     title: string;
     seeAll?: boolean;
 }
-//random number
-const getRandomInt = ( max: number ) => Math.floor( Math.random() * max );
 
 const EventRow = ( { events, title, seeAll } : EventRowProps ) => {
-
+  const isHappeningNow = Array.isArray( events );
   const titleColSpan = seeAll ? "col-span-2" : "col-span-3";
-  const contentColSpan = events.length === 1 ? "col-span-3" : "col-span-1";
+  const contentColSpan = isHappeningNow && events.length === 1 ? "col-span-3" : "col-span-1";
   const seeAllLink = seeAll ? <Link href="/"><p className="text-xs">See all</p></Link> : null;
-  const upcomingRows = events.map( ( event ) => (
-    <div key={getRandomInt( 1000000 )} className={`h-38 ${contentColSpan}`}>
-      <ArticleCardImage event={event} />
-    </div>
-
-  ) );
+  const upcomingRows = isHappeningNow
+    ? events.map( ( event ) => (
+      <div key={event.eventId} className={`h-38 ${contentColSpan}`}>
+        <ArticleCardImage past={event} upcoming={null} />
+      </div>
+    ) )
+    :
+    Array.from( { length: 3 }, ( _, index ) => (
+      <div className={`h-38 ${contentColSpan}`} key={events.past[index].eventId}>
+        <ArticleCardImage past={events.past[index]} upcoming={events.upcoming[index]} />
+      </div>
+    ) );
   return (
     <div className="w-full xl:w-1/2 mx-auto grid px-2 xl:px-36 grid-cols-3 gap-4 p-0 xl:p-8">
       <span className={`h-12 text-3xl truncate font-bold ${titleColSpan}`}>{title}</span>
