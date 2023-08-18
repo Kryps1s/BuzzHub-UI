fc927ec27fd322ce68c2f47c6e2fbb012993d099
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-08-18 16:41:29 -0400

    test

--- __tests__/header.test.js
+++ __tests__/header.test.js
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react'
-import { HeaderTabs } from '../app/components/Header'
+import { HeaderTabs } from 'app/components/Header'
 
 describe('Header', () => {
   it('renders a Happening Today', () => {
     render(<HeaderTabs tabs={["Happening"]}  />)
  
 
-    const happeningToday = screen.getByText(/Log Ign/i)
+    const happeningToday = screen.getByText(/Log In/i)
     expect(happeningToday).toBeInTheDocument()
  
   })
