69af6ef71bbd1c97f1b7c078019273183466b259
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-11-25 14:09:50 -0500

    agenda resized and count badges added

--- app/components/meeting/agenda.tsx
+++ app/components/meeting/agenda.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Accordion } from "@mantine/core";
+import { Accordion, Badge } from "@mantine/core";
 import TaskForm from "./task";
 import { Agenda, Category } from "@/app/lib/types/types";
 
@@ -11,18 +11,21 @@ const MeetingAgenda = ( { agenda } : AgendaProps ) =>
 {
 
   const accordionItems = ( Object.keys( agenda ) as Array<keyof typeof agenda> ).map( ( category ) => (
-    <Accordion key={category}>
+    <Accordion multiple defaultValue={[ "BEEKEEPING", "COLLECTIVE" ]} key={category}>
       <Accordion.Item id={`agenda-category-${category}`} value={`${category}`}>
-        <Accordion.Control>{category}</Accordion.Control>
+        <Accordion.Control>{category} <Badge>{
+        //get length of all subcategories
+          Object.keys( agenda[category] as Category ).reduce( ( acc, subCategory ) => acc + agenda[category][subCategory].length, 0 )
+        }</Badge></Accordion.Control>
         <Accordion.Panel>
-          <Accordion>
+          <Accordion multiple>
             {Object.keys( agenda[category] as Category ).map( ( subCategory, subIndex: number ) => (
               <Accordion.Item
                 id={`agenda-subcategory-${category}-${subIndex}`}
                 value={`${subIndex}`}
                 key={`${subIndex}`}
               >
-                <Accordion.Control>{subCategory}</Accordion.Control>
+                <Accordion.Control>{subCategory} <Badge>{agenda[category][subCategory].length}</Badge></Accordion.Control>
                 <Accordion.Panel>
                   {agenda[category][subCategory].map( ( event, eventIndex: number ) => (
                     <Accordion.Item
@@ -31,7 +34,6 @@ const MeetingAgenda = ( { agenda } : AgendaProps ) =>
                       key={`${eventIndex}`}
                     >
                       <TaskForm task={event} />
-
                     </Accordion.Item>
                   ) )}
                 </Accordion.Panel>
@@ -45,7 +47,7 @@ const MeetingAgenda = ( { agenda } : AgendaProps ) =>
   ) );
 
   return (
-    <Accordion className='overflow-scroll max-h-96'>
+    <Accordion className='overflow-scroll'>
       {accordionItems}
     </Accordion>
   );
