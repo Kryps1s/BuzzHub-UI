c6571cc950ae4bfce4b24284e58e7e40fa5476d5
Elliot O'Reilly <eoreilly1994@gmail.com>
2024-03-10 13:00:10 -0400

    Update eventCard and translationSelector components

--- middleware.tsx
+++ middleware.tsx
@@ -1,9 +1,12 @@
 import createMiddleware from "next-intl/middleware";
 
-import { defaultLocale, locales } from "./i18n";
+import {locales, localePrefix} from './navigation';
 
-export default createMiddleware({locales, defaultLocale});
-
+export default createMiddleware({
+    defaultLocale: 'en',
+    localePrefix,
+    locales
+  });
 export const config = {
     matcher:["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
